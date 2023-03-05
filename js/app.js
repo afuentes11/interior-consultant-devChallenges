115d86f7c8e1a6e978ca75d10cc2c24fac61f584
afuentes11 <afuentes11@cuc.edu.co>
2023-03-05 00:38:22 -0500

    FIX: hamburger menu bug

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-$('.active').on('click', ()=>{
+$('.active, .label').click( ()=>{
     window.scrollTo(0, 0);
     $('.body').toggleClass('body-burger-mode');
     $('.nav').toggleClass('nav-burger-mode');
     $('.logo').toggleClass('logo-burger-mode');
-    console.log('hola')
+    $('#burger').toggleClass('checkbox');
 });
